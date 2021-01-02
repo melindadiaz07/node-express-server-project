9a05bdce5b605a7b8ed1c83346b5816c6eeb7a03
Melindadiaz07 <melindadiaz@me.com>
2021-01-02 14:30:47 -0500

    Added route and html file for chapter 1

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html")
 })
 
+app.get("/chapter-1", (req, res) => {
+  res.sendFile(__dirname + "/chapter1.index")
+})
+
 
 app.listen(3000, () => {
   console.log("Server started on port 3000")
